7b43a6fa3acf60b425647626672a5ca2e4fdd472
Marius <touzeaumarius@live.fr>
2024-07-22 12:07:18 +0200

    adapt link de addLogoLink() pour gh pages

--- scripts/utils/logo.js
+++ scripts/utils/logo.js
@@ -2,7 +2,7 @@ export function addLogoLink() { // Importable dans les autres js
     const logo = document.querySelector(".logo");
     const link = document.createElement('a');
 
-    link.href = '/index.html';
+    link.href = 'index.html';
     link.setAttribute('aria-label', "Fisheye Home page");
     logo.parentNode.insertBefore(link, logo);
     link.appendChild(logo);      
